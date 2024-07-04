854cd37b987d5776edf62442499384bab84d319c
Manuel Pérez <manuelperezm.dev@gmail.com>
2024-07-04 22:55:11 +0200

    Add sonner to card component

--- src/components/CardComponent.tsx
+++ src/components/CardComponent.tsx
@@ -1,40 +1,43 @@
 import React, { useState } from 'react';
 import CopyIcon from '../../public/icons/CopyIcon.jsx';
+import CheckIcon from '../../public/icons/CheckIcon.jsx';
+import { toast, Toaster } from "sonner";
 
 interface CardProps {
   classes?: string;
   text?: string;
 }
-const Card: React.FC<CardProps> = ({ classes }) => {
-  
-  const copyToClipboard = () => {
-    navigator.clipboard.writeText('hello')
-      .then(() => {
-        // toast.success("Copied to clipboard!");
-        console.log('hello')
-      })
-      .catch((err) => {
-        // toast.error("Failed to copy!");
-        console.error('Failed to copy: ', err);
-      });
+
+const Card: React.FC<CardProps> = ({ classes = "", text = "Click me" }) => {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const copyToClipboard = async () => {
+    try {
+      const codeToCopy = `<button type="button" class="${classes}">${text}</button>`;
+      await navigator.clipboard.writeText(codeToCopy);
+      toast.success("Copied to clipboard!");
+      setIsCopied(true);
+      setTimeout(() => setIsCopied(false), 1000); // Reset after 1 second
+    } catch (err) {
+      toast.error("Failed to copy!");
+      console.error("Failed to copy: ", err);
+    }
   };
 
   return (
-    <article
-    className="rounded-2xl bg-indigo-100/80 dark:bg-gray-900/60 z-[1] mx-auto px-4 py-4 sm:h-72 aspect-square border border-indigo-400 dark:border-gray-300/40 shadow"
-  >
-    <section className="relative">
-      <button className="absolute top-0 right-0" onClick={copyToClipboard}>
-        <CopyIcon />
-      </button>
-    </section>
-    <section className="items-center align-center h-full">
-      <div className="flex items-center justify-center h-full">
-        <button type="button" className={classes}> Click me </button>
-      </div>
-    </section>
-
-  </article>
+    <article className="rounded-2xl bg-indigo-100/80 dark:bg-gray-900/60 z-[1] mx-auto px-4 py-4 sm:h-72 aspect-square border border-indigo-400 dark:border-gray-300/40 shadow">
+      <section className="relative">
+        <button className="absolute top-0 right-0" onClick={copyToClipboard}>
+        {isCopied ? <CheckIcon className="text-gray-800  dark:text-indigo-300" /> : <CopyIcon className="text-gray-800 dark:text-indigo-300" />} 
+        </button>
+      </section>
+      <section className="items-center align-center h-full">
+        <div className="flex items-center justify-center h-full">
+          <button type="button" className={classes}>{text}</button>
+        </div>
+      </section>
+      <Toaster richColors /> 
+    </article>
   );
 };
 
