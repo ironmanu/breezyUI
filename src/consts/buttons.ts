2c86c527259de9d874e629a8a8851fdc602ca767
Manuel Pérez <manuelperezm.dev@gmail.com>
2024-07-04 22:54:09 +0200

    Separate const code

---
+++ src/consts/buttons.ts
@@ -0,0 +1,28 @@
+export const buttons = [
+    {
+      text: "Hover me",
+      classes:
+        "relative text-gray-900 dark:text-white bg-transparent border border-gray-600 dark:border-slate-100 h-8 sm:h-12 px-3 sm:px-6 shadow-sm focus:outline-none active:bg-indigo-400 before:content-[''] before:absolute before:inset-0 before:-top-0 before:bg-indigo-300 dark:before:bg-indigo-400  before:-z-10 before:translate-x-2 before:translate-y-2 before:transition-transform before:duration-200 before:ease-out hover:before:translate-x-0 hover:before:translate-y-0",
+    },
+    {
+      text: "Click me",
+      classes:
+        "relative text-gray-900 dark:text-white bg-transparent border border-gray-600 dark:border-slate-100 h-8 sm:h-12 px-3 sm:px-6 shadow-sm focus:outline-none before:content-[''] before:absolute before:inset-0 before:-top-0 before:bg-indigo-300 dark:before:bg-indigo-400 before:-z-10 before:translate-x-2 before:translate-y-2 before:transition-transform before:duration-200 before:ease-out active:before:translate-x-0 active:before:translate-y-0",
+    },
+    {
+      text: "Hover me",
+      classes:
+        "relative text-gray-900 dark:text-white bg-transparent border border-gray-600 dark:border-slate-100 h-8 sm:h-12 px-3 sm:px-6 shadow-sm focus:outline-none before:content-[''] before:absolute before:inset-0 before:-top-0 before:bg-indigo-300/80 before:-z-10 before:translate-x-2 before:-translate-y-2 before:transition-colors before:duration-300 before:ease-in hover:before:bg-indigo-400/80 ",
+    },
+    {
+      text: "Hover me",
+      classes:
+        "relative text-gray-900 rounded-md dark:text-white bg-indigo-300 dark:bg-indigo-400 border border-slate-100 dark:border-gray-800 h-8 sm:h-12 px-3 sm:px-6 shadow-sm focus:outline-none transition-all duration-100 [box-shadow:6px_6px_#334155] dark:[box-shadow:6px_6px_#cbd5e1] hover:translate-x-1 hover:translate-y-1 hover:shadow-none  dark:hover:shadow-none",
+    },
+    {
+      text: "Click me",
+      classes:
+        "relative text-gray-900 rounded-md dark:text-white bg-indigo-300 dark:bg-indigo-400 border border-slate-100 dark:border-gray-700 h-8 sm:h-12 px-3 sm:px-6 shadow-sm focus:outline-none transition-all duration-100 [box-shadow:6px_6px_#334155] dark:[box-shadow:6px_6px_#cbd5e1] active:translate-x-1 active:translate-y-1 active:shadow-none dark:active:shadow-none",
+    },
+
+  ];
