65121edda5823d4b50ff38c8e37c93af0ae4b675
Manuel Pérez <manuelperezm.dev@gmail.com>
2024-07-06 17:04:34 +0200

    add new buttons

--- src/consts/buttons.ts
+++ src/consts/buttons.ts
@@ -1,28 +1,51 @@
 export const buttons = [
     {
+      name: "Shadow up on hover",
       text: "Hover me",
       classes:
         "relative text-gray-900 dark:text-white bg-transparent border border-gray-600 dark:border-slate-100 h-8 sm:h-12 px-3 sm:px-6 shadow-sm focus:outline-none active:bg-indigo-400 before:content-[''] before:absolute before:inset-0 before:-top-0 before:bg-indigo-300 dark:before:bg-indigo-400  before:-z-10 before:translate-x-2 before:translate-y-2 before:transition-transform before:duration-200 before:ease-out hover:before:translate-x-0 hover:before:translate-y-0",
     },
     {
+      name: "Shadow up on click",
       text: "Click me",
       classes:
         "relative text-gray-900 dark:text-white bg-transparent border border-gray-600 dark:border-slate-100 h-8 sm:h-12 px-3 sm:px-6 shadow-sm focus:outline-none before:content-[''] before:absolute before:inset-0 before:-top-0 before:bg-indigo-300 dark:before:bg-indigo-400 before:-z-10 before:translate-x-2 before:translate-y-2 before:transition-transform before:duration-200 before:ease-out active:before:translate-x-0 active:before:translate-y-0",
     },
     {
+      name: "Shadow intesifies on hover",
       text: "Hover me",
       classes:
         "relative text-gray-900 dark:text-white bg-transparent border border-gray-600 dark:border-slate-100 h-8 sm:h-12 px-3 sm:px-6 shadow-sm focus:outline-none before:content-[''] before:absolute before:inset-0 before:-top-0 before:bg-indigo-300/80 before:-z-10 before:translate-x-2 before:-translate-y-2 before:transition-colors before:duration-300 before:ease-in hover:before:bg-indigo-400/80 ",
     },
     {
+      name: "Push button on hover",
       text: "Hover me",
       classes:
         "relative text-gray-900 rounded-md dark:text-white bg-indigo-300 dark:bg-indigo-400 border border-slate-100 dark:border-gray-800 h-8 sm:h-12 px-3 sm:px-6 shadow-sm focus:outline-none transition-all duration-100 [box-shadow:6px_6px_#334155] dark:[box-shadow:6px_6px_#cbd5e1] hover:translate-x-1 hover:translate-y-1 hover:shadow-none  dark:hover:shadow-none",
     },
     {
+      name: "Push button on click",
       text: "Click me",
       classes:
         "relative text-gray-900 rounded-md dark:text-white bg-indigo-300 dark:bg-indigo-400 border border-slate-100 dark:border-gray-700 h-8 sm:h-12 px-3 sm:px-6 shadow-sm focus:outline-none transition-all duration-100 [box-shadow:6px_6px_#334155] dark:[box-shadow:6px_6px_#cbd5e1] active:translate-x-1 active:translate-y-1 active:shadow-none dark:active:shadow-none",
     },
+    {
+      name: "Shadow hiddes on hover",
+      text: "Hover me",
+      classes:
+        "relative text-gray-900 dark:text-white dark:text-white bg-indigo-300 dark:bg-indigo-400 border border-slate-100 dark:border-gray-700 rounded-md h-8 sm:h-12 px-3 sm:px-6 shadow-sm focus:outline-none after:content-[''] after:rounded-md after:absolute after:inset-0 after:bg-gray-800 dark:after:bg-slate-300 after:-z-10 after:translate-x-2 after:translate-y-2 after:transition-transform after:duration-200 after:ease-out hover:after:translate-x-0 hover:after:translate-y-0",
+    },
+    {
+      name: "Shadow hiddes on click",
+      text: "Click me",
+      classes:
+        "relative text-gray-900 dark:text-white dark:text-white bg-indigo-300 dark:bg-indigo-400 border border-slate-100 dark:border-gray-700 rounded-md h-8 sm:h-12 px-3 sm:px-6 shadow-sm focus:outline-none after:content-[''] after:rounded-md after:absolute after:inset-0 after:bg-gray-800 dark:after:bg-slate-300 after:-z-10 after:translate-x-2 after:translate-y-2 after:transition-transform after:duration-200 after:ease-out active:after:translate-x-0 active:after:translate-y-0",
+    },
+    {
+      name: "Border appeares on hover",
+      text: "Hover me",
+      classes:
+        "relative text-gray-900 dark:text-white rounded-md h-8 sm:h-12 px-3 sm:px-6 shadow-xl hover:border-2 hover:border-gray-900 dark:hover:border-slate-100",
+    },
 
-  ];
+]
